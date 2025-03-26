45493668c858b23ee1bfbb0480da7bcedc1d3a46
katariarjunvarma <katariarjunvarma23@lpu.in>
2025-03-26 19:15:15 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from app import app
 
+# Entry point of the application
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Run the application
+    # Ensure debug mode is only enabled in development
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Set debug=False in production
